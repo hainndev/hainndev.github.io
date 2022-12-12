bcd24440ff2067ec21ba3f9b2ef039f45aa876d1
hainndev <hainn.dev@gmail.com>
2022-12-12 17:42:38 +0700

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  SOCKET_URL: "wss://datafeed-widget.dnse.com.vn/wss",
+  CHART_API_URL: "https://services.entrade.com.vn/chart-api/v2",
+  ENV: "production",
+  NODE_ENV: "production",
+  PRICE_BOARD_URL: "https://banggia.dnse.com.vn",
+}
